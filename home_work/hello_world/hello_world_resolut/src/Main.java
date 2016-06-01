1dd627ce96ae0d87dc5f7a6ea27d29ab481bf232
SD-Redolut <samdimus@yandex.ru>
2016-06-01 18:06:52 +0300

    Homework 1. Hello-World

---
+++ home_work/hello_world/hello_world_resolut/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @author resolut
+ * @version 1.0.0
+ * created 01.06.2016
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+}
