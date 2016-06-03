b1706165a1e2eda8806bb2f7ade8538d0b1e5734
dns2316 <dns2316@gmail.com>
2016-06-03 20:35:34 +0300

    tablex

---
+++ home_work/tablex/src/table/a2.java
@@ -0,0 +1,13 @@
+package table;
+
+/************************************
+ * Created by dns2316 on 03.06.2016.*
+ ***********************************/
+public class a2 {
+
+    public static int[] create(int x){
+        int[] tablex = new int[9];
+        for (int i = 1; i<10; i++) tablex[i] = System.out.println(x + " * " + i + " = " + x * i);
+        return tablex;
+    }
+}
---
+++ home_work/tablex/src/table/main.java
@@ -0,0 +1,12 @@
+package table;
+
+/************************************
+ * Created by dns2316 on 03.06.2016.*
+ ***********************************/
+public class main {
+    public static void main(String[] args){
+        a2 aa = new a2();
+        aa.create(2);
+    }
+
+}
